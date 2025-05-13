aabee1d32e4736cd58a197d23771bca478afd10c
Ubukaze1 <gabo2060@hotmail.com>
2025-05-13 00:51:35 -0500

    feat(header): se termino el header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Toolbar } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
+import { SharedModule } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonModule, AvatarModule,SharedModule,Toolbar],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
 }
