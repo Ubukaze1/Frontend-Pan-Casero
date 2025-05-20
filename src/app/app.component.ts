e7548d2a2d5bb4efc73d722af5a2482f81461b4d
Ubukaze1 <gabo2060@hotmail.com>
2025-05-20 00:19:48 -0500

    Router listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import {InventarioComponent} from './components/inventario/inventario.component';
 import {ProductService} from '../service/product.service';
-import {ProductosComponent} from './components/productos/productos.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, InventarioComponent, ProductosComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ProductService],
