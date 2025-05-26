d518b57b75a473e451260665d5e87c8feae8724c
Ubukaze1 <gabo2060@hotmail.com>
2025-05-25 21:20:12 -0500

    fix(mainpage): se cambio muchas cosas y se creo el componente card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import {ProductService} from '../service/product.service';
-import {MainpageComponent} from './components/mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainpageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ProductService],
--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
+import {CardComponent} from './card/card.component';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css',
 })
