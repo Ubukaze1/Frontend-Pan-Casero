6ab57471a3d7763a6d32bdd4bf8d583ec390bbdc
Ubukaze1 <gabo2060@hotmail.com>
2025-05-26 13:45:57 -0500

    feat(modal): modal listo pero aun falta

--- src/app/components/mainpage/card/card.component.ts
+++ src/app/components/mainpage/card/card.component.ts
@@ -1,22 +1,41 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { Tienda } from '../../../../interface/product';
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
-  @Input() sty = ""
-  @Input() img = ""
-  @Input() prod = "Producto"
+  @Input() sty = '';
+  @Input() img = '';
+  @Input() prod!: Tienda;
   ngOnInit() {}
 
-  @Output() verMas: EventEmitter<any> = new EventEmitter()
-
-  onVerMasClick() {
-    this.verMas.emit()
+  onVerMasClick(produ: Tienda) {
+    console.log(produ);
+    console.log('VerMasClick');
   }
 
+  @ViewChild('cardDialog') dialogRef!: ElementRef<HTMLDialogElement>;
+
+  ngAfterViewInit() {
+    // Asegúrate de que el <dialog> existe antes de manipularlo.
+    // Nada que hacer aquí explícitamente, solo que Angular lo inicialice.
+  }
+
+  abrirModal() {
+    // .showModal() abre el <dialog> en pantalla
+    this.dialogRef.nativeElement.showModal();
+  }
+
+  cerrarModal() {
+    this.dialogRef.nativeElement.close();
+  }
 }
--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,7 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import {HeaderComponent} from '../mainpage/header/header.component';
 import {FooterComponent} from '../mainpage/footer/footer.component';
 import {CardComponent} from '../mainpage/card/card.component';
+import {Tienda} from '../../../interface/product';
 
 @Component({
   selector: 'app-tienda',
@@ -11,4 +12,22 @@ import {CardComponent} from '../mainpage/card/card.component';
 })
 export class TiendaComponent {
   wandh = "width: 300px ;height: 235px; border-radius: 0px;"
+  products!: Tienda[];
+
+  ngOnInit() {
+    this.products = [
+      { id: 1, nombre: 'Pan', precio: 4000, description: "Un buenn pan1", img: "/Pan1.jpg" },
+      { id: 2, nombre: 'Pan', precio: 4000, description: "Un buenn pan2", img: "/Pan1.jpg" },
+      { id: 3, nombre: 'Pan', precio: 4000, description: "Un buenn pan3", img: "/Pan1.jpg" },
+      { id: 4, nombre: 'Pan', precio: 4000, description: "Un buenn pan4", img: "/Pan1.jpg" },
+      { id: 5, nombre: 'Pan', precio: 4000, description: "Un buenn pan5", img: "/Pan1.jpg" },
+      { id: 6, nombre: 'Pan', precio: 4000, description: "Un buenn pan6", img: "/Pan1.jpg" },
+      { id: 7, nombre: 'Pan', precio: 4000, description: "Un buenn pan7", img: "/Pan1.jpg" },
+      { id: 8, nombre: 'Pan', precio: 4000, description: "Un buenn pan8", img: "/Pan1.jpg" },
+      { id: 9, nombre: 'Pan', precio: 4000, description: "Un buenn pan9", img: "/Pan1.jpg" },
+      { id: 10, nombre: 'Pan', precio: 4000, description: "Un buenn pan10", img: "/Pan1.jpg" },
+      { id: 11, nombre: 'Pan', precio: 4000, description: "Un buenn pan11", img: "/Pan1.jpg" },
+      { id: 12, nombre: 'Pan', precio: 4000, description: "Un buenn pan12", img: "/Pan1.jpg" },
+    ];
+  }
 }
