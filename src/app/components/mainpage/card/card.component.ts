e459d9a1a632d70efb432057f9f53f6bbbf6d305
Ubukaze1 <gabo2060@hotmail.com>
2025-05-25 23:00:12 -0500

    fix(mainpage): se termino la main page

--- src/app/components/mainpage/card/card.component.ts
+++ src/app/components/mainpage/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+
+  @Input() img = ""
+  @Input() prod = "Producto"
+  ngOnInit() {}
 
 }
--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {CardComponent} from './card/card.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [HeaderComponent, CardComponent],
+  imports: [HeaderComponent, CardComponent, FooterComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css',
 })
